dc210bea21af3e7ab8dd9851b059596a6d774946
Tilak Poudel <tilak@dzangolab.com>
2021-05-10 22:22:26 +0545

    fix: minor bugs

--- src/components/BreadCrumb/Breadcrumb.js
+++ src/components/BreadCrumb/Breadcrumb.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { brown } from '../../colors';
-import './BreadCrumbStyle.css'
+import React from "react";
+import { brown } from "../../colors";
+import "./BreadCrumbStyle.css";
 
-const breadcrumb={
-    backgroundColor:"transparent",
-    borderRadius:'0.37'
-}
-const Breadcrumb=(props)=>{
-    function isLast(index){
-        return index===props.crumbs.length-1;
-    }
-    return(
-        // <nav className="row">
-            <nav className="mainbody-section nopadding text-center">
-        <ol className="breadcrumb" style={breadcrumb}>
-        {
-            props.crumbs.map((crumb,ci)=>{
-                const disabled=isLast(ci)?"disabled" : "";
-                return(
-                   <button className={`btn btn-link ${disabled} `} onClick={()=>props.selected(crumb)}>
-                    {crumb}
-                </button>
-        
-                )
-            })
-        }
-        </ol>
-        </nav>
-    )
-
-}
+const breadcrumb = {
+  backgroundColor: "transparent",
+  borderRadius: "0.37",
+};
+const Breadcrumb = (props) => {
+  function isLast(index) {
+    return index === props.crumbs.length - 1;
+  }
+  return (
+    // <nav className="row">
+    <nav className="mainbody-section nopadding text-center">
+      <ol className="breadcrumb" style={breadcrumb}>
+        {props.crumbs.map((crumb, ci) => {
+          const disabled = isLast(ci) ? "disabled" : "";
+          return (
+            <button
+              key={ci}
+              className={`btn btn-link ${disabled} `}
+              onClick={() => props.selected(crumb)}
+            >
+              {crumb}
+            </button>
+          );
+        })}
+      </ol>
+    </nav>
+  );
+};
 export default Breadcrumb;
